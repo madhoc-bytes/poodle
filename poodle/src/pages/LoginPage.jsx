04d320d5b499d0983217e07759861b97fd8b53b4
andy <huangandy05@gmail.com>
2023-06-22 12:52:27 +1000

    fixed login and register page - implemented form checking

--- poodle/src/pages/LoginPage.jsx
+++ poodle/src/pages/LoginPage.jsx
@@ -1,53 +1,15 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Button, FormControl, InputLabel, Input, Typography, Paper, Box, Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100vh',
-  },
-  title: {
-    marginBottom: theme.spacing(4),
-  },
-  roleSelect: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginBottom: theme.spacing(2),
-  },
-  roleOption: {
-    flex: 1,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    cursor: 'pointer',
-  },
-  selectedRole: {
-    backgroundColor: 'pink',
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    maxWidth: 300,
-    marginBottom: theme.spacing(2),
-  },
-  formControl: {
-    marginBottom: theme.spacing(2),
-  },
-}));
+// import { useHistory } from 'react-router-dom';
+import { CssBaseline, Button, FormControl, InputLabel, Input, Typography, Paper, Box, Link, Container } from '@mui/material';
 
 const LoginPage = () => {
-  const classes = useStyles();
-  const history = useHistory();
+  // const history = useHistory();
   const [role, setRole] = useState('Student');
 
   const handleLogin = () => {
     // You might want to validate input or connect to your backend here
-    history.push('/dummy-route');
+    console.log('pressed login yes');
+    // history.push('/dummy-route');
   };
 
   const handleRoleSelect = (newRole) => {
@@ -55,38 +17,80 @@ const LoginPage = () => {
   };
 
   return (
-    <Box className={classes.root}>
-      <Typography variant="h2" className={classes.title}>Poodle</Typography>
-      <Typography variant="h4">Login</Typography>
-      <Paper className={classes.roleSelect}>
-        <Box
-          className={`${classes.roleOption} ${role === 'Student' ? classes.selectedRole : ''}`}
-          onClick={() => handleRoleSelect('Student')}
+    <Container component='main' maxWidth='xs'>
+      <Box
+        sx={{
+          display: 'flex',
+          // marginTop: 10,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          // minHeight: '100vh',
+          gap: 2,
+          backgroundColor: '#D8BFD8',
+          width: '25rem',
+          height: '40rem',
+          boxShadow: 4,
+          borderRadius: 5,
+        }}
+      >
+        <Typography variant="h2" sx={{ mb: 4 }}>Poodle</Typography>
+        <Typography variant="h4">Login</Typography>
+        <Paper
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            mb: 2,
+            '& div': {
+              flex: 1,
+              p: 2,
+              textAlign: 'center',
+              cursor: 'pointer',
+              '&:first-of-type': {
+                bgcolor: role === 'Student' ? 'pink' : 'inherit',
+              },
+              '&:last-of-type': {
+                bgcolor: role === 'Teacher' ? 'pink' : 'inherit',
+              },
+            },
+          }}
         >
-          Student
-        </Box>
+          <Box onClick={() => handleRoleSelect('Student')}>
+            Student
+          </Box>
+          <Box onClick={() => handleRoleSelect('Teacher')}>
+            Teacher
+          </Box>
+        </Paper>
         <Box
-          className={`${classes.roleOption} ${role === 'Teacher' ? classes.selectedRole : ''}`}
-          onClick={() => handleRoleSelect('Teacher')}
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            maxWidth: 300,
+            mb: 2,
+            '& > :not(:last-child)': {
+              mb: 2, // This replaces the marginBottom from makeStyles
+            },
+          }}
+          onSubmit={handleLogin}
         >
-          Teacher
+          <FormControl>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" type="email" />
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input id="password" type="password" />
+          </FormControl>
+          <Button variant="contained" color="primary" type="submit">Login</Button>
         </Box>
-      </Paper>
-      <form className={classes.form} onSubmit={handleLogin}>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <Input id="email" type="email" />
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <Input id="password" type="password" />
-        </FormControl>
-        <Button variant="contained" color="primary" type="submit">Login</Button>
-      </form>
-      <Typography>
-        Don't have an account? <Link href="/register" variant="body1" style={{ fontWeight: 'bold', textDecoration: 'underline' }}>Register now</Link>
-      </Typography>
-    </Box>
+        <Typography>
+          Don't have an account? <Link href="/register" variant="body1" sx={{ fontWeight: 'bold', textDecoration: 'underline' }}>Register now</Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
