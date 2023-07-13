85b4e4060e0fdc8040bc6c5daa3bfd572255e67b
Hua Hu <z5313381@ad.unsw.edu.au>
2023-07-13 15:12:30 +1000

    changes

--- poodle/backend/courses/assignment.py
+++ poodle/backend/courses/assignment.py
@@ -6,7 +6,7 @@ from variables import secret_key
 from werkzeug.exceptions import BadRequest, Unauthorized, NotFound
 import os
 
-def create_assignment(user_id, course_id, title, description, due_date, max_marks):
+def create(user_id, course_id, title, description, due_date, max_marks):
 	user = User.query.get(user_id)
 	
 	if not user.is_teacher:
@@ -39,19 +39,6 @@ def create_assignment(user_id, course_id, title, description, due_date, max_mark
 
 	return jsonify({'message': 'Assignment created successfully', 'assignment_id': new_assignment.id}), 201
 
-""" TO DO: Upload Spec, 
-- Incorporate Update spec, replace file, destinations
-
-TO DO: Submit Assignment, 
-- Incorporate resubmission, replace file, destination
-
-TO DO: Update Mark, 
-
-TO DO: Retrieve Mark, 
-
-TO DO: Retrieve Submission (Filter, retrieve last submission)
-Get all submissions from assignment_id, filter by student_id, sort in desc, and grab latest.
-"""
 
 def upload_spec(user_id, course_id, assignment_id, spec):
 	user = User.query.get(user_id)
@@ -121,6 +108,7 @@ def submit(user_id, course_id, assignment_id, submission_file):
 	
 	return jsonify({'message': 'Assignment successfully submitted at ' + current_time, 'file_id': file.id}), 201
 
+
 def update_score(user_id, submission_id, score):
 	user = User.query.get(user_id)
 	
@@ -138,6 +126,7 @@ def update_score(user_id, submission_id, score):
 
 	return jsonify({'message': 'Score updated successfully'}), 201
 
+
 def retrieve_score(user_id, submission_id):
 	user = User.query.get(user_id)
 	
@@ -147,6 +136,7 @@ def retrieve_score(user_id, submission_id):
 	submission = User.query.get(submission_id)
 
 	return jsonify({'message': 'Score successfully retrieved', 'submission_mark': submission.mark}), 201
+
 
 def retrieve_submission(user_id, assignment_id, student_id):
 	user = User.query.get(user_id)
@@ -162,65 +152,4 @@ def retrieve_submission(user_id, assignment_id, student_id):
 	submission = Submission.query.filter_by(student_id=student_id, assignment_id = assignment_id).first()
 
 	return get_file(submission.file_id)
-
-def get_assignments_course(user_id, course_id):
-	user = User.query.get(user_id)
-	course = Course.query.get(course_id)
-	ass_list = []
-
-	if not user:
-		raise NotFound('User not found')
-	
-	if not course:
-		raise NotFound('Course not found')
-	
-	# Return list of assignments for teacher
-	if user.is_teacher:
-		# Check teacher is creator of course
-		if course.creator != user_id:
-			raise Unauthorized('User permission denied')
-		
-		asses = Assignment.query.filter_by(creator=user_id, course_id=course_id).all()
-		
-		for ass in asses:
-			ass_list.append({'title' : ass.title, 'id': ass.ass_id, 'due_date': ass.due_date})
-	# Return list of assignments for student
-	else:
-		# Check student is in course
-		course_enrolments = db.session.query(Course).join(Enrolment, Course.id == Enrolment.course_id).filter(Enrolment.user_id == user_id).all()
-
-		if course not in course_enrolments:
-			raise Unauthorized('User permission denied')
-
-		asses = Assignment.query.filter_by(creator=user_id, course_id=course_id).all()
-		
-		for ass in asses:
-			ass_list.append({'title' : ass.title, 'id': ass.ass_id, 'due_date': ass.due_date})
-
-	return jsonify(ass_list), 200
-
-def all_ass(course_id):
-	# Check if the course exists
-	course = Course.query.get(course_id)
-	if not course:
-		return jsonify({'message': 'Course not found'}), 404
-
-	# Retrieve the online classes for the given course
-	ass = Assignment.query.filter_by(course_id=course_id).all()
-	ass_schema = AssignmentSchema()
-	return ass_schema.jsonify(ass, many=True), 200
-
-def get_ass_name(course_id, ass_id):
-	ass = Assignment.query.get(ass_id)
-	if not ass:
-		raise NotFound('Assignment not found')
-	
-	course = Course.query.get(course_id)
-	if not course:
-		raise NotFound('Course not found')
-	
-	if ass.course_id != course_id:
-		raise Unauthorized('User permission denied')
-	
-	return jsonify({'name': ass.name}), 200
 
