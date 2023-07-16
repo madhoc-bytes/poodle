0805137303753eac42344b08f60573a7f83a8487
Jeffery Pan <jdpan002@gmail.com>
2023-07-16 14:57:20 +1000

    changed submission so that it can't be submitted past due date

--- poodle/backend/courses/assignment.py
+++ poodle/backend/courses/assignment.py
@@ -81,11 +81,18 @@ def upload_spec(user_id, course_id, assignment_id, spec_file):
 def submit(user_id, course_id, assignment_id, submission_file):
 	user = User.query.get(user_id)
 	
+	# teachers can't make submissions
 	if user.is_teacher:
 		raise Unauthorized('User permission denied')
 	
-	submission = Submission.query.filter_by(student_id=user_id, assignment_id = assignment_id).first()
+	# cannot submit past the due date
+	assignment = Assignment.query.get(assignment_id)
+	if assignment.due_date < datetime.now():
+		raise BadRequest('Assignment due date has passed')
 
+	submission = Submission.query.filter_by(student_id=user_id, assignment_id = assignment_id)
+
+	# if submission already exists, delete the old file and replace it with the new one
 	if not submission:
 		current_time = datetime.now()
 		file = File(folder_id=0, name=str(assignment_id).join(str(user_id)), date_created=current_time, file_path="")
@@ -102,7 +109,7 @@ def submit(user_id, course_id, assignment_id, submission_file):
 
 		file.file_path = destination
 		db.session.commit()
-
+	# else, create a new submission
 	else:
 		file = File.query.get(submission.file_id)
 		destination = file.file_path
